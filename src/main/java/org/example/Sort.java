af2c67ce8fd3a1c7ef1757095ef334437e6d015d
DimitrovaZori <ub01632@gmailcom>
2024-02-21 18:29:34 +0200

    .

--- src/main/java/org/example/Sort.java
+++ src/main/java/org/example/Sort.java
@@ -1,2 +1,23 @@
-package org.example;public class Sort {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+
+public class Sort {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(10);
+        numbers.add(12);
+
+
+        Collections.sort(numbers);
+        System.out.println(numbers);
+    }
 }
